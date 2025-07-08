43c802db9c932d42cc20b4223b864ec0b4853535
Durgesh Kumar Gupta <158087807+durgeshkumargupta1210s@users.noreply.github.com>
2025-07-08 18:33:31 +0530

    Time: 0 ms (100.00%) | Memory: 48.1 MB (28.04%) - LeetSync

---
+++ 275-h-index-ii/h-index-ii.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        return indexfound(citations);
+    }
+    public static int  indexfound(int[] ci) {
+		// TODO Auto-generated method stub
+		// Arrays.sort(ci);
+		int count=0;
+		int lo=0;
+		int hi=ci.length-1;
+		while(lo<=hi) {
+			int mid=(lo+hi)/2;
+			int h=ci.length-mid;
+			if(h<=ci[mid]) {
+				count=h;
+ 				hi=mid-1; // for higher index 
+			}
+			else {
+				lo=mid+1;
+			}
+			
+			
+		}
+		
+		return count;
+    }
+}
