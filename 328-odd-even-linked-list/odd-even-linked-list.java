bd828b78261dc27649de82283abc004e05c8aaa5
Durgesh Kumar Gupta <158087807+durgeshkumargupta1210s@users.noreply.github.com>
2025-07-11 23:01:19 +0530

    Time: 0 ms (100.00%) | Memory: 44.5 MB (79.40%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.java
+++ 328-odd-even-linked-list/odd-even-linked-list.java
@@ -1,50 +1,22 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public ListNode oddEvenList(ListNode head) {
-        if(head==null || head.next==null){
+        if (head == null || head.next == null) {
             return head;
         }
-        ArrayList<Integer> list=new ArrayList<>();
 
-        ListNode temp=head;
-        while(temp!=null && temp.next!=null){
-            list.add(temp.val);
-            temp=temp.next.next;
-        }
-        if(temp!=null){
-            list.add(temp.val);
-        }
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode evenHead = even;
 
+        while (even != null && even.next != null) {
+            odd.next = even.next;
+            odd = odd.next;
 
-         temp=head.next;
-        while(temp!=null && temp.next!=null){
-            list.add(temp.val);
-            temp=temp.next.next;
-        }
-        if(temp!=null){
-            list.add(temp.val);
+            even.next = odd.next;
+            even = even.next;
         }
 
-
-         temp=head;
-        int i=0;
-        while(temp!=null){
-            temp.val=list.get(i);
-            i++;
-            temp=temp.next;
-        }
+        odd.next = evenHead;
         return head;
-
-
-        
     }
 }
