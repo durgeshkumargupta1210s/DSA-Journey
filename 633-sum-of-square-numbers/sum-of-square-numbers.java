5944d84e1ef639a9927019a3da8dfb6b54976757
Durgesh Kumar Gupta <158087807+durgeshkumargupta1210s@users.noreply.github.com>
2025-08-06 00:11:40 +0530

    Time: 4 ms (55.72%) | Memory: 40.8 MB (18.49%) - LeetSync

---
+++ 633-sum-of-square-numbers/sum-of-square-numbers.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean judgeSquareSum(int c) {
+        return squareSum(c);
+    }
+
+    private static boolean squareSum(int c) {
+        if (c < 0) {
+            return false;
+        }
+        
+        int left = 0;
+        int right = (int) Math.sqrt(c);
+
+        while (left <= right) {
+          long sum = (long) left * left + (long) right * right;
+
+            if (sum == c) {
+                return true;
+            } else if (sum < c) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return false;
+    }
+}
