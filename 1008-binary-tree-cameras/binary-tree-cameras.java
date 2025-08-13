f16e8d18214de69428225b5ab0d5c4af5e99f8c7
Durgesh Kumar Gupta <158087807+durgeshkumargupta1210s@users.noreply.github.com>
2025-08-13 11:35:51 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (58.21%) - LeetSync

---
+++ 1008-binary-tree-cameras/binary-tree-cameras.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int camera=0;
+
+    public int minCameraCover(TreeNode root) {
+        int c=minCamera(root);
+        if(c==-1){
+            camera++;
+        }
+        return camera;
+
+    }
+    public int minCamera(TreeNode root){
+        if(root==null){
+            return 0;
+        }
+        int left=minCamera(root.left);
+        int right=minCamera(root.right);
+        if(left==-1 || right==-1){ // is node camera ki need hai
+            camera++;
+            return 1; // camera setup done on node
+        }
+        else if(left==1 || right==1){ // enmese kishi ek ke pass cameara hai ya phir dono ke pass 
+           return 0;// alraedy coverded
+        }
+
+        else{
+            return -1; // i need camrea ...camera requirement
+        }
+    }
+}
