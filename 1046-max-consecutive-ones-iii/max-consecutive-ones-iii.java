7700539dc1607eeb83aaef9fa3bf2e5f73009bac
Durgesh Kumar Gupta <158087807+durgeshkumargupta1210s@users.noreply.github.com>
2025-08-22 00:35:45 +0530

    Time: 3 ms (94.61%) | Memory: 53 MB (24.69%) - LeetSync

---
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int longestOnes(int[] nums, int k) {
+        return longestsubarray(nums,k);
+    }
+
+    public static int longestsubarray(int[] nums, int k) {
+		// TODO Auto-generated method stub
+		int si=0,ans=0,count=0;
+		for(int ei=0; ei<nums.length; ei++) {
+			if(nums[ei]==0) {
+				count++;
+			}
+			while(count>k) {
+				if(nums[si]==0) {
+					count--;
+				}
+				si++;
+			}
+			ans=Math.max(ans,ei-si+1);
+		}
+		return ans;
+		
+	}
+}
