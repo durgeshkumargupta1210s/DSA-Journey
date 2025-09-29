b9a3db52dd2398af2a7d3d462bb51e0bc67009e1
Durgesh Kumar Gupta <158087807+durgeshkumargupta1210s@users.noreply.github.com>
2025-09-29 15:52:37 +0530

    Time: 8 ms (99.44%) | Memory: 64.4 MB (10.43%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -1,34 +1,35 @@
-import java.util.*;
-
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        if (nums == null || k <= 0) {
-            return new int[0];
-        }
-        int n = nums.length;
-        int[] result = new int[n - k + 1];
-        Deque<Integer> dq = new ArrayDeque<>();
-       // storing index instead of value
+        int n=nums.length;
+        int [] prefixMax=new int[n];
+        int [] suffixMax=new int [n];
+        int [] ans=new int[n-k+1];
 
-        for (int i = 0; i < nums.length; i++) {
-            // Remove indices that are out of this window
-            while (!dq.isEmpty() && dq.peekFirst() < i - k + 1) {
-                dq.pollFirst();
+        prefixMax[0]=nums[0];
+        for(int i=0; i<n; i++){
+            if(i%k==0){
+                prefixMax[i]=nums[i];
             }
-
-            // Remove from back while current element is greater than elements at those indices
-            while (!dq.isEmpty() && nums[dq.peekLast()] < nums[i]) {
-                dq.pollLast();
-            }
-
-            // Add current index at back
-            dq.offerLast(i);
-
-            // If we have formed at least one full window, record the maximum
-            if (i >= k - 1) {
-                result[i - k + 1] = nums[dq.peekFirst()];
+            else{
+                prefixMax[i]=Math.max(prefixMax[i-1],nums[i]);
             }
         }
-        return result;
+
+        suffixMax[n-1]=nums[n-1];
+
+        for(int i=n-2; i>=0; i--){
+            if((i+1)%k==0){
+                suffixMax[i]=nums[i];
+            }
+            else{
+                suffixMax[i]=Math.max(suffixMax[i+1],nums[i]);
+            }
+        }
+
+        for(int i=0; i<ans.length; i++){
+            ans[i]=Math.max(suffixMax[i],prefixMax[i+k-1]);
+        }
+        return ans;
+        
     }
 }
