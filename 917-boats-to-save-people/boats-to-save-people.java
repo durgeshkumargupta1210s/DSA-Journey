38df6adbba754babb5f8924423613389965f730a
Durgesh Kumar Gupta <158087807+durgeshkumargupta1210s@users.noreply.github.com>
2025-10-02 09:43:10 +0530

    Time: 16 ms (94.77%) | Memory: 54.7 MB (86.28%) - LeetSync

--- 917-boats-to-save-people/boats-to-save-people.java
+++ 917-boats-to-save-people/boats-to-save-people.java
@@ -1,27 +1,26 @@
 class Solution {
     public int numRescueBoats(int[] people, int limit) {
-       return  numberOfRescueBoats(people,limit);
+        Arrays.sort(people);
+
+        int pairCount=0;
+        int singleCount=0;
+
+        int left=0;
+        int right=people.length-1;
+
+        while(left<=right){
+            if(people[left]+people[right]<=limit){
+                pairCount++;
+                left++;
+                right--;
+            }
+            else{
+                right--;
+                singleCount++;
+            }
+        }
+
+        return singleCount+pairCount;
+        
     }
-    private static int numberOfRescueBoats(int[] people, int limit) {
-		// TODO Auto-generated method stub
-		int sumCount=0;
-		int count=0;
-		Arrays.sort(people);
-		int left=0;
-		int right=people.length-1;
-		while(left<=right) {
-			if(people[left]+people[right]<=limit) {
-				sumCount++;
-				left++;
-				right--;
-			}
-			else {
-				right--;
-				count++;
-			}
-			
-		}
-		return sumCount + count;
-		
-	}
 }
