971110b8ff54a87f7e44a41eb2ff16f125338674
Durgesh Kumar Gupta <158087807+durgeshkumargupta1210s@users.noreply.github.com>
2025-10-06 09:27:21 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (78.37%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.java
+++ 486-predict-the-winner/predict-the-winner.java
@@ -3,26 +3,30 @@ class Solution {
         int left=0;
         int right=nums.length-1;
         int total=0;
+        int [][] dp=new int [nums.length+1][nums.length+1];
         for(int i=0; i<nums.length; i++){
             total+=nums[i];
         }
 
-        int p1=OptimalGameStrategy(nums,left,right);
+        int p1=OptimalGameStrategy(nums,left,right,dp);
         int p2=total-p1;
 
         return p1>=p2;
         
     }
-    public int OptimalGameStrategy(int [] nums, int left, int right){
+    public int OptimalGameStrategy(int [] nums, int left, int right, int [][] dp){
         if(left>right){
             return 0;
         }
+        if(dp[left][right]!=0){
+            return dp[left][right];
+        }
 
-        int choose_left=nums[left]+Math.min(OptimalGameStrategy(nums,left+2,right), OptimalGameStrategy(nums,left+1, right-1));
+        int choose_left=nums[left]+Math.min(OptimalGameStrategy(nums,left+2,right, dp), OptimalGameStrategy(nums,left+1, right-1, dp));
 
-        int choose_right=nums[right]+Math.min(OptimalGameStrategy(nums,left+1,right-1), OptimalGameStrategy(nums,left, right-2));
+        int choose_right=nums[right]+Math.min(OptimalGameStrategy(nums,left+1,right-1, dp), OptimalGameStrategy(nums,left, right-2, dp));
 
-        return Math.max(choose_left, choose_right);
+        return dp[left][right]= Math.max(choose_left, choose_right);
 
 
     }
