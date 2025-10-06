842c5f8c17e6ba6d32e89dc1b8de8912f4d3831c
Durgesh Kumar Gupta <158087807+durgeshkumargupta1210s@users.noreply.github.com>
2025-10-06 16:08:21 +0530

    Time: 47 ms (21.09%) | Memory: 40.6 MB (41.26%) - LeetSync

--- 526-beautiful-arrangement/beautiful-arrangement.java
+++ 526-beautiful-arrangement/beautiful-arrangement.java
@@ -1,23 +1,52 @@
 class Solution {
-    int count=0;
+    // Global variable to keep track of the total number of valid arrangements
+    int count = 0;
+
+    /**
+     * Main function to count the number of beautiful arrangements.
+     * @param n the number from 1 to n to arrange
+     * @return the total number of valid arrangements
+     */
     public int countArrangement(int n) {  
-        boolean[] visited=new boolean[n];
-        int position=1;
-        GeneratePermutation(n,position,visited);
+        // visited[i] will be true if number (i+1) is already used in the current arrangement
+        boolean[] visited = new boolean[n];
+
+        // Start generating arrangements from position 1
+        GeneratePermutation(n, 1, visited);
+
+        // Return the total count of valid arrangements found
         return count; 
     }
     
-    public void GeneratePermutation(int n ,int position,boolean[] visited){
-        if(position>n){
-           count++;
+    /**
+     * Recursive backtracking function to generate valid arrangements.
+     * @param n total number of elements to arrange
+     * @param position current position being filled (1-indexed)
+     * @param visited boolean array to track which numbers are already used
+     */
+    public void GeneratePermutation(int n, int position, boolean[] visited) {
+        // Base case: if all positions have been filled (position > n)
+        // it means we found a valid arrangement
+        if (position > n) {
+            count++;
             return;
         }
         
-        for(int i=1; i<=n; i++){
-            if(!visited[i-1] && (i%position==0 || position%i==0)){
-                visited[i-1]=true;
-                GeneratePermutation(n,position+1,visited);
-                visited[i-1]=false;
+        // Try placing each number (1 to n) at the current position
+        for (int i = 1; i <= n; i++) {
+            // Check if number 'i' is not already used
+            // AND it satisfies the beautiful arrangement condition:
+            // (number divisible by position OR position divisible by number)
+            if (!visited[i - 1] && (i % position == 0 || position % i == 0)) {
+
+                // Choose the number 'i' for this position
+                visited[i - 1] = true;
+
+                // Recurse for the next position
+                GeneratePermutation(n, position + 1, visited);
+
+                // Backtrack: unmark number 'i' to use it in other permutations
+                visited[i - 1] = false;
             }
         }
     }
