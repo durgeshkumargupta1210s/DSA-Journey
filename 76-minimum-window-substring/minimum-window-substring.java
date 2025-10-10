59b4555fe8dc29e734a7e1d61f48d15d2176bf20
Durgesh Kumar Gupta <158087807+durgeshkumargupta1210s@users.noreply.github.com>
2025-10-10 11:05:13 +0530

    Time: 16 ms (66.21%) | Memory: 44.8 MB (72.99%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,42 +1,37 @@
 class Solution {
     public String minWindow(String s, String t) {
-        if(s.length()<t.length()){
-            return "";
-        }
-        HashMap<Character, Integer> map=new HashMap<>();
-        for(int i=0; i<t.length(); i++){
-            char ch=t.charAt(i);
-            map.put(ch,  map.getOrDefault(ch,0)+1);
+        if(s.length()<t.length()) return "";
+
+        Map<Character, Integer> map=new HashMap<>();
+        for(char ch : t.toCharArray()){
+            map.put(ch,map.getOrDefault(ch,0)+1);
         }
 
-        int si=0;
-        int sub_si=0;
-        int ans=Integer.MAX_VALUE;
-        int ei=0;
-        int rc=t.length();
-        while(ei<s.length()){
-            char ch=s.charAt(ei);
-            if(map.containsKey(ch)&&map.get(ch)>0)
-            rc--;
+        int si=0,  need=t.length(), ans=Integer.MAX_VALUE,sub_si=0;
 
-            map.put(ch,map.getOrDefault(ch,0)-1);
-            
-           
-            while(rc==0){
-               char sc=s.charAt(si);
-               map.put(sc,map.get(sc)+1);
+        for(int ei=0; ei<s.length(); ei++){
+           char ch=s.charAt(ei);
 
-               if(ans>ei-si+1){
+           if(map.containsKey(ch) && map.get(ch)>0){
+              need--;   
+           }
+           map.put(ch,map.getOrDefault(ch,0)-1);
+
+            while(need==0){
+                char ch1=s.charAt(si);
+                if(ans>ei-si+1){
                 ans=ei-si+1;
                 sub_si=si;
-               }
-               if(map.get(sc)>0){
-                rc++;
-               } 
-               si++;
+                }
+                map.put(ch1,map.getOrDefault(ch1,0)+1);
+
+                if(map.get(ch1)>0){
+                    need++;
+                }
+                si++;
             }
-            ei++;
+           
         }
-        return ans==Integer.MAX_VALUE?"":s.substring(sub_si,sub_si+ans);
+        return ans==Integer.MAX_VALUE?"":s.substring(sub_si,sub_si+ans);   
     }
 }
