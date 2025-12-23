051fb129f71950dc8970849015b80158b844a64e
Durgesh Kumar Gupta <158087807+durgeshkumargupta1210s@users.noreply.github.com>
2025-12-23 23:25:41 +0530

    Time: 11 ms (89.74%) | Memory: 51.3 MB (33.33%) - LeetSync

--- 2828-lexicographically-smallest-string-after-substring-operation/lexicographically-smallest-string-after-substring-operation.java
+++ 2828-lexicographically-smallest-string-after-substring-operation/lexicographically-smallest-string-after-substring-operation.java
@@ -1,24 +1,25 @@
 class Solution {
     public String smallestString(String s) {
-        char[] arr = s.toCharArray();
-        int n = arr.length;
+        int n=s.length();
+        char [] arr=s.toCharArray();
 
-        int i = 0;
-
-        while (i < n && arr[i] == 'a') {
+        int i=0;
+        while(i<n && arr[i]=='a'){
             i++;
         }
 
-        if (i == n) {
-            arr[n - 1] = 'z';
+        if(i==n){
+            arr[n-1]='z';
             return new String(arr);
         }
 
-        while (i < n && arr[i] != 'a') {
-            arr[i] = (char)(arr[i] - 1);
+        while(i<n && arr[i]!='a'){
+            arr[i]=(char)(arr[i]-1);
             i++;
         }
 
         return new String(arr);
+
+        
     }
 }
