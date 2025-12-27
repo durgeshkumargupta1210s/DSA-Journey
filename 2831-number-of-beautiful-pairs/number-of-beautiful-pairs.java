893a930deccd0f1890f102b732cf26376b866d6b
Durgesh Kumar Gupta <158087807+durgeshkumargupta1210s@users.noreply.github.com>
2025-12-27 22:46:52 +0530

    Time: 12 ms (86.13%) | Memory: 46.5 MB (43.23%) - LeetSync

---
+++ 2831-number-of-beautiful-pairs/number-of-beautiful-pairs.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int countBeautifulPairs(int[] nums) {
+        int count = 0;
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+           
+            int firstDigit = getFirstDigit(nums[i]);
+
+            for (int j = i + 1; j < n; j++) {
+           
+                int lastDigit = nums[j] % 10;
+
+                if (gcd(firstDigit, lastDigit) == 1) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+
+    private int getFirstDigit(int num) {
+        while (num >= 10) {
+            num /= 10;
+        }
+        return num;
+    }
+
+  
+    private int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+}
