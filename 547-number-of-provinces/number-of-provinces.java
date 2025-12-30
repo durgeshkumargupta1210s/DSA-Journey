9972dd111c85a2a2eef2d342f3bda589f05e8261
Durgesh Kumar Gupta <158087807+durgeshkumargupta1210s@users.noreply.github.com>
2025-12-30 15:40:59 +0530

    Time: 0 ms (100.00%) | Memory: 47.5 MB (34.24%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,27 +1,43 @@
 class Solution {
-    public int findCircleNum(int[][] isConnected) {
-        int row=isConnected.length;
-        int ans=0;
-        boolean[] visited=new boolean[row];
 
-        for(int i=0; i<row; i++){
-            if(!visited[i]){
-                ans++;
-                dfs( isConnected, visited, i);
+    // Main function to count the number of provinces (connected components)
+    public int findCircleNum(int[][] isConnected) {
+
+        // Number of cities (nodes)
+        int row = isConnected.length;
+
+        // This will store the final count of provinces
+        int ans = 0;
+
+        // Visited array to track which cities have already been explored
+        boolean[] visited = new boolean[row];
+
+        // Iterate over each city
+        for (int i = 0; i < row; i++) {
+
+            // If the city is not visited, it starts a new province
+            if (!visited[i]) {
+                ans++;                      // New province found
+                dfs(isConnected, visited, i); // Explore all connected cities
             }
         }
+
         return ans;
-        
     }
 
-    public void dfs(int [][] grid,boolean[] visited, int node){
-        
-        visited[node]=true;
-        for(int i = 0 ; i < grid.length ; i++){
-            if(!visited[i] && grid[node][i] == 1){
-                dfs(grid , visited , i);
+    // Depth-First Search to visit all cities connected to the current city
+    public void dfs(int[][] grid, boolean[] visited, int node) {
+
+        // Mark the current city as visited
+        visited[node] = true;
+
+        // Explore all possible connections from this city
+        for (int i = 0; i < grid.length; i++) {
+
+            // If city i is connected and not yet visited
+            if (!visited[i] && grid[node][i] == 1) {
+                dfs(grid, visited, i); // Recursively visit connected city
             }
         }
-
     }
 }
