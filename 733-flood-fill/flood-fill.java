d4afc3615bfaec9bde578bd25c9c324cd60c6545
Durgesh Kumar Gupta <158087807+durgeshkumargupta1210s@users.noreply.github.com>
2025-12-30 13:17:38 +0530

    Time: 0 ms (100.00%) | Memory: 46.6 MB (87.22%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,35 +1,22 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int rows=image.length;
-        int cols=image[0].length;
 
-        if(image[sr][sc]==color){
-            return image;
-        }
-
-        int originalcolor=image[sr][sc];
-        int [] []directions={{1,0},{-1,0}, {0,1}, {0,-1}};
-        image[sr][sc]=color;
-
-        Queue<int[]> q=new LinkedList<>();
-
-        q.add(new int []{sr,sc});
-
-        while(!q.isEmpty()){
-            int [] current=q.poll();
-
-            for(int [] dir : directions){
-                int newrow=current[0]+dir[0];
-                int newcol=current[1]+dir[1];
-
-                if(newrow>=0 &&  newcol>=0 && newrow<rows && newcol<cols && image[newrow][newcol] == originalcolor
- ){
-                    image[newrow][newcol] =color;
-                    q.offer(new int[]{newrow, newcol});
-                }
-            }
-        }
+        newfloodFill(image,sr,sc,color,image[sr][sc]);
         return image;
         
+    }
+    public void newfloodFill(int[][] image, int sr, int sc, int color,int original){
+        if(sr<0 || sc<0 || sr>=image.length || sc>=image[0].length ){
+            return;
+        }
+        if(image[sr][sc]!=original || image[sr][sc]==color){
+            return;
+        }
+        image[sr][sc]=color;
+        newfloodFill(image, sr+1,sc,color, original);
+        newfloodFill(image, sr-1,sc,color, original);
+        newfloodFill(image, sr,sc+1,color, original);
+        newfloodFill(image, sr,sc-1,color, original);
+
     }
 }
