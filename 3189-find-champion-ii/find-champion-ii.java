ba31b6c533610bb6c102fc9b00eb9d67375dec61
Durgesh Kumar Gupta <158087807+durgeshkumargupta1210s@users.noreply.github.com>
2026-01-04 00:59:33 +0530

    Time: 5 ms (17.98%) | Memory: 48.1 MB (58.05%) - LeetSync

--- 3189-find-champion-ii/find-champion-ii.java
+++ 3189-find-champion-ii/find-champion-ii.java
@@ -1,26 +1,23 @@
 class Solution {
     public int findChampion(int n, int[][] edges) {
-
-        int[] indegree = new int[n];
-
-        // Step 1: Calculate indegrees
-        for (int[] edge : edges) {
-            int weaker = edge[1];
-            indegree[weaker]++;
+        Set<Integer> set=new HashSet<>();
+        ArrayList<Integer> list=new ArrayList<>();
+        for(int [] arr : edges){
+            set.add(arr[1]);
         }
 
-        // Step 2: Find unique node with indegree 0
-        int champion = -1;
-        int countZero = 0;
-
-        for (int i = 0; i < n; i++) {
-            if (indegree[i] == 0) {
-                champion = i;
-                countZero++;
+        for(int i=0; i<n; i++){
+            if(!set.contains(i)){
+                list.add(i);
             }
         }
+        if(list.size()>=2){
+            return -1;
+        }
+        return list.get(0);
+        
 
-        // Step 3: Validate uniqueness
-        return countZero == 1 ? champion : -1;
+
+        
     }
 }
