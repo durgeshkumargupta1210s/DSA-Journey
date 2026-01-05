67360716924ec34a35a89507116366b1b522c7db
Durgesh Kumar Gupta <158087807+durgeshkumargupta1210s@users.noreply.github.com>
2026-01-05 23:01:09 +0530

    Time: 5 ms (60.44%) | Memory: 46.9 MB (57.14%) - LeetSync

--- 2191-minimum-number-of-food-buckets-to-feed-the-hamsters/minimum-number-of-food-buckets-to-feed-the-hamsters.java
+++ 2191-minimum-number-of-food-buckets-to-feed-the-hamsters/minimum-number-of-food-buckets-to-feed-the-hamsters.java
@@ -1,34 +1,54 @@
 class Solution {
-    public int minimumBuckets(String hamsters) {
-        char[] arr = hamsters.toCharArray();
-        int n = arr.length;
-        int buckets = 0;
 
+    public int minimumBuckets(String hamsters) {
+
+        // Length of the input string
+        int n = hamsters.length();
+
+        // Convert string to character array for easy modification
+        char[] arr = hamsters.toCharArray();
+
+        // Counter to store number of buckets placed
+        int bucket = 0;
+
+        // Traverse the string from left to right
         for (int i = 0; i < n; i++) {
+
+            // Process only if the current position has a hamster
             if (arr[i] == 'H') {
 
-                // Check if already fed by left bucket
+                // Case 1:
+                // If the left position already has a bucket,
+                // then this hamster is already fed
                 if (i - 1 >= 0 && arr[i - 1] == 'B') {
                     continue;
                 }
 
-                // Prefer placing bucket on the right
+                // Case 2 (Greedy choice):
+                // Prefer placing a bucket on the right side if possible
+                // because it may also feed the next hamster
                 if (i + 1 < n && arr[i + 1] == '.') {
-                    arr[i + 1] = 'B';
-                    buckets++;
+                    arr[i + 1] = 'B';   // Place bucket
+                    bucket++;           // Increment bucket count
                 }
-                // Else place on the left
+
+                // Case 3:
+                // If right side is not available, try placing on the left
                 else if (i - 1 >= 0 && arr[i - 1] == '.') {
-                    arr[i - 1] = 'B';
-                    buckets++;
+                    arr[i - 1] = 'B';   // Place bucket
+                    bucket++;           // Increment bucket count
                 }
-                // Impossible to feed this hamster
+
+                // Case 4:
+                // If neither left nor right side is available,
+                // it is impossible to feed this hamster
                 else {
                     return -1;
                 }
             }
         }
 
-        return buckets;
+        // Return the minimum number of buckets placed
+        return bucket;
     }
 }
