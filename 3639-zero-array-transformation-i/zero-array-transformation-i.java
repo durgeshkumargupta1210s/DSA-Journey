d22483f3aa0a869294aa5bbd80feff5aac48f5b0
Durgesh Kumar Gupta <158087807+durgeshkumargupta1210s@users.noreply.github.com>
2026-01-08 22:41:31 +0530

    Time: 3 ms (90.24%) | Memory: 220.5 MB (54.36%) - LeetSync

--- 3639-zero-array-transformation-i/zero-array-transformation-i.java
+++ 3639-zero-array-transformation-i/zero-array-transformation-i.java
@@ -1,28 +1,44 @@
 class Solution {
+
     public boolean isZeroArray(int[] nums, int[][] queries) {
-        int n = nums.length;
-        int[] diff = new int[n + 1];
 
-      
-        for (int[] q : queries) {
-            int l = q[0];
-            int r = q[1];
+        // Difference array to efficiently apply range increments
+        // Size is nums.length + 1 to safely handle r + 1 index
+        int[] diff = new int[nums.length + 1];
 
-            diff[l]++;
-            if (r + 1 < n) {
-                diff[r + 1]--;
+        // Process each query [l, r]
+        // Each query allows us to decrement nums[l..r] by 1
+        for (int[] arr : queries) {
+            int l = arr[0];  // left index
+            int r = arr[1];  // right index
+
+            // Start decrement effect at index l
+            diff[l] += 1;
+
+            // End decrement effect after index r
+            if (r + 1 < nums.length) {
+                diff[r + 1] -= 1;
             }
         }
 
-  
-        int curr = 0;
-        for (int i = 0; i < n; i++) {
-            curr += diff[i];
-            if (curr < nums[i]) {
+        // Variable to store prefix sum of difference array
+        // This represents total decrements available at index i
+        int count = 0;
+
+        // Traverse the nums array
+        for (int i = 0; i < nums.length; i++) {
+
+            // Accumulate the difference array
+            count += diff[i];
+
+            // If available decrements are less than nums[i],
+            // we cannot reduce nums[i] to zero
+            if (count < nums[i]) {
                 return false;
             }
         }
 
+        // All elements can be reduced to zero
         return true;
     }
 }
