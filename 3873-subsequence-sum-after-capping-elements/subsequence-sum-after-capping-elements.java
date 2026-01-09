214cf0d3decb2e2dec8c847ca7a2a5dff26f9e25
Durgesh Kumar Gupta <158087807+durgeshkumargupta1210s@users.noreply.github.com>
2026-01-09 18:10:25 +0530

    Time: 96 ms (90.43%) | Memory: 46.1 MB (100.00%) - LeetSync

--- 3873-subsequence-sum-after-capping-elements/subsequence-sum-after-capping-elements.java
+++ 3873-subsequence-sum-after-capping-elements/subsequence-sum-after-capping-elements.java
@@ -1,30 +1,68 @@
 import java.util.Arrays;
 
 class Solution {
+
+    /**
+     * For each x from 1 to n:
+     *  - Cap all elements in nums to at most x
+     *  - Check if there exists a subsequence whose sum equals k
+     *  - Store the result in ans[x - 1]
+     */
     public boolean[] subsequenceSumAfterCapping(int[] nums, int k) {
+
         int n = nums.length;
         boolean[] ans = new boolean[n];
 
+        // Sort the array so that elements < x can be processed incrementally
         Arrays.sort(nums);
 
+        /*
+         * dp[s] = true if we can form sum 's' using
+         * elements strictly smaller than the current cap 'x'
+         */
         boolean[] dp = new boolean[k + 1];
-        dp[0] = true;
+        dp[0] = true; // Base case: sum 0 is always achievable
 
-        int i=0;
+        // Pointer to track how many elements are < x
+        int i = 0;
 
+        /*
+         * Iterate over each possible cap value x (1 to n)
+         */
         for (int x = 1; x <= n; x++) {
 
+            /*
+             * Include all elements strictly smaller than x into the DP
+             * Each such element contributes its original value
+             */
             while (i < n && nums[i] < x) {
                 int val = nums[i];
+
+                // Standard 0/1 knapsack update (backward to avoid reuse)
                 for (int s = k; s >= val; s--) {
                     dp[s] |= dp[s - val];
                 }
                 i++;
             }
 
+            /*
+             * Elements from index i to n-1 are >= x,
+             * after capping they all become exactly x
+             */
             int ncapped = n - i;
+
+            /*
+             * We can use at most:
+             *  - ncapped elements (availability)
+             *  - k / x elements (to not exceed sum k)
+             */
             int maxUse = Math.min(ncapped, k / x);
 
+            /*
+             * Try using j capped elements (each contributes x)
+             * Remaining sum = k - j*x
+             * Check if that remainder is achievable using dp[]
+             */
             for (int j = 0; j <= maxUse; j++) {
                 if (dp[k - j * x]) {
                     ans[x - 1] = true;
@@ -32,6 +70,7 @@ class Solution {
                 }
             }
         }
+
         return ans;
     }
 }
