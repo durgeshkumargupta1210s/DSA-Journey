7ce0232cfaf4fec7b3167984e921ee00c6594614
Durgesh Kumar Gupta <158087807+durgeshkumargupta1210s@users.noreply.github.com>
2026-01-27 00:23:25 +0530

    Time: 0 ms (100.00%) | Memory: 44.2 MB (10.81%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,27 +1,43 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
 
-        int i=m-1;// nums1 pointer
-        int j=n-1;// nums2 pointer
-        int k=m+n-1;// used for element putting in the last of nums1
+        // Pointer i starts from the last valid element in nums1
+        int i = m - 1;
 
-        while(i>=0 && j>=0){
-            if(nums1[i]<nums2[j]){
-                nums1[k]=nums2[j];
-                k--;
-                j--;
+        // Pointer j starts from the last element in nums2
+        int j = n - 1;
+
+        // Pointer k starts from the last position of nums1 (total size = m + n)
+        int k = m + n - 1;
+
+        // Merge nums1 and nums2 from the back
+        // Continue until either nums1 or nums2 is completely processed
+        while (i >= 0 && j >= 0) {
+
+            // If current element in nums2 is larger,
+            // place it at the end of nums1
+            if (nums1[i] < nums2[j]) {
+                nums1[k] = nums2[j];
+                j--;  // Move nums2 pointer left
             }
-            else{
-                nums1[k]=nums1[i];
-                k--; 
-                i--;
+
+            // Otherwise, place current element of nums1 itself
+            else {
+                nums1[k] = nums1[i];
+                i--;  // Move nums1 pointer left
             }
-        }
-        while(j>=0){
-            nums1[k]=nums2[j];
+
+            // Move the final position pointer left
             k--;
-            j--;
         }
-        
+
+        // If nums2 still has remaining elements,
+        // copy them into nums1
+        // (No need to copy nums1 leftovers because they are already in place)
+        while (j >= 0) {
+            nums1[k] = nums2[j];
+            j--;  // Move nums2 pointer left
+            k--;  // Move merged pointer left
+        }
     }
 }
