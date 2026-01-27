6ff07e16f6aacb68f3cb5698abe1abe9909bf87b
Durgesh Kumar Gupta <158087807+durgeshkumargupta1210s@users.noreply.github.com>
2026-01-27 21:34:36 +0530

    Time: 0 ms (100.00%) | Memory: 46.4 MB (12.92%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -1,14 +1,24 @@
 class Solution {
+
+    // Function to calculate maximum profit with unlimited transactions
     public int maxProfit(int[] prices) {
+
+        // Variable to store total profit
         int profit = 0;
-        
+
+        // Traverse through stock prices starting from day 1
         for (int i = 1; i < prices.length; i++) {
-            // If today's price is higher than yesterday's, take the profit
+
+            // If today's price is greater than yesterday's price
+            // then we can make profit by buying yesterday and selling today
             if (prices[i] > prices[i - 1]) {
+
+                // Add the profit of this transaction
                 profit += prices[i] - prices[i - 1];
             }
         }
-        
+
+        // Return total accumulated profit
         return profit;
     }
 }
