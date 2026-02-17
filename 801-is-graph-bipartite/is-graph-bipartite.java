2512a975fcd72f76e18ba3900de85a02849acf48
Durgesh Kumar Gupta <158087807+durgeshkumargupta1210s@users.noreply.github.com>
2026-02-18 01:33:09 +0530

    Time: 13 ms (6.69%) | Memory: 47.1 MB (89.95%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.java
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -1,47 +1,50 @@
 class Solution {
+    List<List<Integer>> list;
     public boolean isBipartite(int[][] graph) {
-        Queue<BipartitePair> q=new LinkedList<>();
-			HashMap<Integer, Integer> visited=new HashMap<>();
-			for(int i=0; i<graph.length; i++) { // vtx
-				if(visited.containsKey(i)) {
-					continue;
-				}
-				q.add(new BipartitePair(i,0));
-				
-				while(!q.isEmpty()) {
-				   // 1.Remove 
-					BipartitePair rp=q.poll();
-				// 2.Ignore if Already visited
-					if(visited.containsKey(rp.vtx)){
-						if(visited.get(rp.vtx)!=rp.dis) {
-							return false;
-						}
-						
-					}
-					// 3.marked visited
-					visited.put(rp.vtx, rp.dis);
-					
-					// 4. self work
-					// 5. Add unvisited nbrs
-					
-					for(int nbrs:graph[rp.vtx]) {
-						if(!visited.containsKey(nbrs)) {
-							q.add(new BipartitePair(nbrs, rp.dis+1));
-						}
-					}
-				}
-			}
-		return true;
+        list=new ArrayList<>();
+        int n=graph.length;
+
+        for(int i=0; i<n; i++ ){
+            list.add(new ArrayList<>());
+        }
+
+        for(int i=0; i<graph.length; i++){
+            int u=i;
+            for(int j=0; j<graph[i].length; j++){
+                int v=graph[i][j];
+                list.get(u).add(v);
+                list.get(v).add(u);
+
+            }
+        }
+
+        
+        int [] color=new int[n+1];
+        Arrays.fill(color,-1);
+
+        for(int i=0; i<n; i++){
+            if(color[i]==-1){
+                Queue<Integer> q=new LinkedList<>();
+                q.add(i);
+                color[i]=0;
+
+                while(!q.isEmpty()){
+                    int node=q.poll();
+
+                    for(int nbrs : list.get(node)){
+                         
+                         if(color[nbrs]==color[node]){
+                            return false;
+                         }
+
+                         if(color[nbrs]==-1){
+                            color[nbrs]=1-color[node];
+                            q.add(nbrs);
+                         }
+                    }
+                }
+            }
+        }
+        return true;
     }
-
-    class BipartitePair{
-		int vtx;
-		int dis;
-		
-		public BipartitePair(int vtx, int dis) {
-			this.vtx=vtx;
-			this.dis=dis;
-		}
-	}
 }
-
