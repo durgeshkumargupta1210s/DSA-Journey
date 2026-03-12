73e2ac282d9a2a066082310274df5c820926600e
Durgesh Kumar Gupta <158087807+durgeshkumargupta1210s@users.noreply.github.com>
2026-03-12 07:12:54 +0530

    Time: 14 ms (31.88%) | Memory: 46.1 MB (37.54%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,22 +1,31 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        return removespecialcharacter(s);
+
+        // Remove all non-alphanumeric characters from the string
+        // and convert the string to lowercase for case-insensitive comparison
+        String str = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+
+        // Two pointers: i starts from beginning, j starts from end
+        int i = 0;
+        int j = str.length() - 1;
+
+        // Traverse the string from both ends towards the center
+        while(i < j){
+
+            // If characters at both pointers are not equal,
+            // then the string is not a palindrome
+            if(str.charAt(i) != str.charAt(j)){
+                return false;
+            }
+
+            // Move the left pointer forward
+            i++;
+
+            // Move the right pointer backward
+            j--;
+        }
+
+        // If all characters matched, the string is a palindrome
+        return true;
     }
-    private static Boolean removespecialcharacter(String str) {
-		// TODO Auto-generated method stub
-		String s=str.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
-		 int i = 0;
-		 int j= s.length()-1;
-		 while(i<j) {
-			 if(s.charAt(i)!=s.charAt(j)) {
-				 return false;
-			 }
-			 i++;
-			 j--;
-			 
-		 }
-		
-		return true;
-		
-	}
 }
