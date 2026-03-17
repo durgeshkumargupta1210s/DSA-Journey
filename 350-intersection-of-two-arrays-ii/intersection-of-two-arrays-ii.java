748ac8f9d25fc46c583968a8ec4a3d7e932f64f8
Durgesh Kumar Gupta <158087807+durgeshkumargupta1210s@users.noreply.github.com>
2026-03-17 15:27:40 +0530

    Time: 3 ms (95.18%) | Memory: 45 MB (65.50%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.java
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.java
@@ -1,28 +1,40 @@
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-        return intersection_array(nums1,nums2);
+
+        // HashMap to store frequency of elements from nums1
+        // key   -> element
+        // value -> number of times it appears
+        Map<Integer, Integer> map = new HashMap<>();
+
+        // List to store the intersection elements
+        List<Integer> list = new ArrayList<>();
+
+        // Step 1: Count frequency of each number in nums1
+        for(int i = 0; i < nums1.length; i++){
+            map.put(nums1[i], map.getOrDefault(nums1[i], 0) + 1);
+        }
+
+        // Step 2: Traverse nums2 and check if element exists in map
+        for(int i = 0; i < nums2.length; i++){
+
+            // If the element exists and its frequency is still positive
+            if(map.containsKey(nums2[i]) && map.get(nums2[i]) > 0){
+
+                // Add it to the intersection list
+                list.add(nums2[i]);
+
+                // Decrease its frequency since it is used once
+                map.put(nums2[i], map.get(nums2[i]) - 1);
+            }
+        }
+
+        // Step 3: Convert the list into an array
+        int[] result = new int[list.size()];
+        for(int i = 0; i < list.size(); i++){
+            result[i] = list.get(i);
+        }
+
+        // Return the final intersection array
+        return result;
     }
-   private static int[] intersection_array(int[] nums1, int[] nums2) {
-		// TODO Auto-generated method stub
-		 Map<Integer, Integer> map = new HashMap<>();
-	        List<Integer> result = new ArrayList<>();
-	        
-	        for (int ele : nums1) {
-	            map.put(ele, map.getOrDefault(ele, 0) + 1);
-	        }
-	        
-	        for (int num : nums2) {
-	            if (map.containsKey(num) && map.get(num) > 0) {
-	                result.add(num);
-	                map.put(num, map.get(num) - 1); 
-	            }
-	        }
-	     
-	        int[] temp = new int[result.size()];
-	        for (int i = 0; i < result.size(); i++) {
-	            temp[i] = result.get(i);
-	        }
-	        
-	        return temp;
-	}
 }
