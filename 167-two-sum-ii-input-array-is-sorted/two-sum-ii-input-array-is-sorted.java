7751862ab9453be1470095a5621e768fc3490f0a
Durgesh Kumar Gupta <158087807+durgeshkumargupta1210s@users.noreply.github.com>
2026-03-17 10:46:27 +0530

    Time: 2 ms (96.34%) | Memory: 48.5 MB (56.34%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
@@ -1,24 +1,40 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        return indexfound(numbers,target);
+
+        // Variable to store the sum of two numbers
+        int sum = 0;
+
+        // Left pointer starts from the beginning of the array
+        int left = 0;
+
+        // Right pointer starts from the end of the array
+        int right = numbers.length - 1;
+
+        // Continue searching until the two pointers meet
+        while (left < right) {
+
+            // Calculate the current sum of elements at left and right pointers
+            sum = numbers[left] + numbers[right];
+
+            // If the sum equals the target, we found the required pair
+            if (sum == target) {
+                break;
+            }
+
+            // If the sum is smaller than the target,
+            // move the left pointer to increase the sum
+            else if (sum < target) {
+                left++;
+            }
+
+            // If the sum is greater than the target,
+            // move the right pointer to decrease the sum
+            else {
+                right--;
+            }
+        }
+
+        // Return indices in 1-based indexing as required by the problem
+        return new int[]{left + 1, right + 1};
     }
-    public static int[] indexfound(int[] arr, int target) {
-		// TODO Auto-generated method stub
-		int i=0;
-		int j=arr.length-1;
-		int sum=0;
-		while(i<j) {
-			 sum=arr[i]+arr[j];
-			if(sum==target) {
-				break;
-			}
-			else if(sum>target) {
-				j--;
-			}
-			else {
-				i++;
-			}
-		}
-		return new int[] {i+1,j+1} ;
-	}
 }
