e56265c88b22b45dd7c7386ea08bb1a03d7a9e3f
Durgesh Kumar Gupta <158087807+durgeshkumargupta1210s@users.noreply.github.com>
2026-06-28 16:41:14 +0530

    Time: 3 ms (56.62%) | Memory: 47.1 MB (39.53%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,36 +1,16 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map=new HashMap<>();
+        for(int i=0; i<nums.length; i++){
+            int complement=target-nums[i];
 
-        // HashMap to store number and its index
-        // Key   -> number from array
-        // Value -> index of that number
-        Map<Integer, Integer> map = new HashMap<>();
-
-        // Length of the input array
-        int n = nums.length;
-
-        // Traverse the array
-        for (int i = 0; i < n; i++) {
-
-            // Calculate the number required to reach the target
-            // Example: target = 9, nums[i] = 2 → second = 7
-            int second = target - nums[i];
-
-            // Check if the required number already exists in the map
-            if (map.containsKey(second)) {
-
-                // If found, return the indices of the two numbers
-                // map.get(second) gives index of the complement
-                return new int[]{i, map.get(second)};
+            if(map.containsKey(complement)){
+                return new int[]{map.get(complement),i};
             }
 
-            // Store the current number with its index in the map
-            // This allows future elements to find it as their complement
-            map.put(nums[i], i);
+            map.put(nums[i],i);
         }
-
-        // If no valid pair is found (problem usually guarantees one),
-        // return default values
-        return new int[]{0, 0};
+        return new int[] {0,0};
+        
     }
 }
