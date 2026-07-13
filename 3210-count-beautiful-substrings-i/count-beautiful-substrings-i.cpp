040a4b61517a2c65aa5a37a0edba64af38724193
Durgesh Kumar Gupta <158087807+durgeshkumargupta1210s@users.noreply.github.com>
2026-07-13 21:02:43 +0530

    Time: 31 ms (78.55%) | Memory: 9.8 MB (97.58%) - LeetSync

---
+++ 3210-count-beautiful-substrings-i/count-beautiful-substrings-i.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int beautifulSubstrings(string s, int k) {
+        int count=0;
+        for(int i=0; i<s.length(); i++){
+            int vowel=0;
+            int cons=0;
+
+            for(int j=i; j<s.length(); j++){
+                char ch=s[j];
+                if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u'){
+                    vowel++;
+                }
+                else{
+                    cons++;
+                }
+
+                if((vowel==cons) && (vowel*cons)%k==0){
+                    count++;
+                }
+            }
+        }
+        return count;
+        
+    }
+};
