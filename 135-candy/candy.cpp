91e9a4bbc2a7e3ee5f908561506bbe2ac11fd8c3
Durgesh Kumar Gupta <158087807+durgeshkumargupta1210s@users.noreply.github.com>
2026-07-14 00:05:08 +0530

    Time: 3 ms (24.36%) | Memory: 23.1 MB (30.98%) - LeetSync

---
+++ 135-candy/candy.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n=ratings.size();
+        vector<int> candy(n);
+        for(int i=0; i<n; i++){
+            candy[i]=1;
+        }
+
+        for(int i=1; i<n; i++){
+            if(ratings[i]>ratings[i-1]){
+                candy[i]=candy[i-1]+1;
+            }
+        }
+
+        for(int i=n-2; i>=0; i--){
+            if(ratings[i]>ratings[i+1]){
+                candy[i]=max(candy[i],candy[i+1]+1);
+            }
+        }
+
+        int count=0;
+
+        for(int ele : candy){
+            count+=ele;
+        }
+
+        return count;
+        
+    }
+};
