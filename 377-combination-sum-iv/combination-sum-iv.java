ff6015f466ad54467d1da683601dbd97a5602db8
Durgesh Kumar Gupta <158087807+durgeshkumargupta1210s@users.noreply.github.com>
2026-08-02 20:26:53 +0530

    Time: 3 ms (13.01%) | Memory: 42.4 MB (90.87%) - LeetSync

---
+++ 377-combination-sum-iv/combination-sum-iv.java
@@ -0,0 +1,33 @@
+class Solution {
+
+    public int combinationSum4(int[] nums, int target) {
+        int[] dp = new int[target + 1];
+
+        for (int i = 0; i <= target; i++) {
+            dp[i] = -1;
+        }
+
+        return ways(nums, target, dp);
+    }
+
+    private int ways(int[] nums, int target, int[] dp) {
+        if (target == 0) {
+            return 1;
+        }
+
+        if (dp[target] != -1) {
+            return dp[target];
+        }
+
+        int ans = 0;
+
+        for (int num : nums) {
+            if (target >= num) {
+                ans += ways(nums, target - num, dp);
+            }
+        }
+
+        dp[target] = ans;
+        return ans;
+    }
+}
