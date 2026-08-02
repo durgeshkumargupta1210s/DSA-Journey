ae0fe583cf6a9dd72bab9406f1e5c6e1e5ed3001
Durgesh Kumar Gupta <158087807+durgeshkumargupta1210s@users.noreply.github.com>
2026-08-02 22:57:03 +0530

    Time: 129 ms (80.38%) | Memory: 42.7 MB (95.27%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,41 +1,37 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[0].length; j++) {
-                if (board[i][j] == word.charAt(0)) {
-                    if (isWordExist(board, i, j, word, 0)) {
+        int m=board.length;
+        int n=board[0].length;
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(board[i][j]==word.charAt(0)){
+                    if(isWordExist(i,j,board,word,0)){
                         return true;
                     }
                 }
             }
         }
         return false;
+        
     }
-
-    public static boolean isWordExist(char[][] board, int cr, int cc, String word, int idx) {
-        // ✅ if we matched full word
-        if (idx == word.length()) {
+    public static boolean isWordExist(int cr, int cc, char[][] board, String word, int idx){
+        if(idx==word.length()){
             return true;
         }
 
-        // boundary + mismatch check
-        if (cr < 0 || cc < 0 || cr >= board.length || cc >= board[0].length 
-            || board[cr][cc] != word.charAt(idx)) {
+        if(cr<0 || cc<0 || cr>=board.length || cc >=board[0].length || board[cr][cc]!=word.charAt(idx) ){
             return false;
         }
 
-        // mark as visited
-        char temp = board[cr][cc];
-        board[cr][cc] = '#';
+        char temp=board[cr][cc];
+        board[cr][cc]='#';
 
-        // explore all directions
-        boolean found = isWordExist(board, cr - 1, cc, word, idx + 1) ||
-                        isWordExist(board, cr + 1, cc, word, idx + 1) ||
-                        isWordExist(board, cr, cc - 1, word, idx + 1) ||
-                        isWordExist(board, cr, cc + 1, word, idx + 1);
+        boolean found=isWordExist(cr+1,cc,board,word,idx+1)||
+                    isWordExist(cr-1,cc,board,word,idx+1)||
+                    isWordExist(cr,cc+1,board,word,idx+1)||
+                    isWordExist(cr,cc-1,board,word,idx+1);
 
-        // backtrack
-        board[cr][cc] = temp;
+        board[cr][cc]=temp;
 
         return found;
     }
