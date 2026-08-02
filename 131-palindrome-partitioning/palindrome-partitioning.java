2d9d16bcad7dc9ab89278b5b5d35fa5455ee9303
Durgesh Kumar Gupta <158087807+durgeshkumargupta1210s@users.noreply.github.com>
2026-08-02 21:08:28 +0530

    Time: 9 ms (25.76%) | Memory: 65.1 MB (88.33%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.java
+++ 131-palindrome-partitioning/palindrome-partitioning.java
@@ -1,39 +1,36 @@
 class Solution {
+    static List<List<String>> ans=new ArrayList<>();
     public List<List<String>> partition(String s) {
-        List<String>ll=new ArrayList<>();
-		List<List<String>> ans=new ArrayList<>();
-		partitions(s,ll,ans);
-		return ans;
+        ans.clear(); 
+        solve(s,new ArrayList<>());
+        return ans;
         
     }
-    private static void partitions(String ques,List<String> ll,List<List<String>> ans) {
-		// TODO Auto-generated method stub
-		if(ques.length()==0) {
-			ans.add(new ArrayList(ll));
-			return;
-		}
-		for(int i=1; i<=ques.length(); i++) {
-			String s=ques.substring(0,i);
-			if(ispalindrom(s)) {
-				ll.add(s);
-				partitions(ques.substring(i),ll,ans);
-				ll.remove(ll.size()-1);
-			}
-			
-		}
-		
-	}
-	private static boolean ispalindrom(String s) {
-		// TODO Auto-generated method stub
-		int i=0;
-		int j=s.length()-1;
-		while(i<j) {
-			if(s.charAt(i)!=s.charAt(j)) {
-				return false;
-			}
-			i++;
-			j--;
-		}
-		return true;
-	}
+    public static void solve(String s , List<String> list){
+        if(s.length()==0){
+            ans.add(new ArrayList<>(list));
+            return;
+        }
+
+        for(int i=1; i<=s.length(); i++){
+            String a=s.substring(0,i);
+            if(isPalindrome(a)){
+                list.add(a);
+                solve(s.substring(i),list);
+                list.remove(list.size()-1);
+            }
+        }
+    }
+    public static boolean isPalindrome(String s){
+        int i=0; 
+        int j=s.length()-1;
+        while(i<j){
+            if(s.charAt(i)!=s.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
 }
