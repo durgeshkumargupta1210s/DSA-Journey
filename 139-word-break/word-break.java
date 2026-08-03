0e6455c9fab4ea1b888fb81975d37a8510c988ee
Durgesh Kumar Gupta <158087807+durgeshkumargupta1210s@users.noreply.github.com>
2026-08-03 13:18:26 +0530

    Time: 6 ms (68.73%) | Memory: 46.6 MB (11.90%) - LeetSync

--- 139-word-break/word-break.java
+++ 139-word-break/word-break.java
@@ -1,35 +1,28 @@
-import java.util.*;
-
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
-        Set<String> dict = new HashSet<>(wordDict);
-
-        int[] dp = new int[s.length()];
-        Arrays.fill(dp, -1);
-
-        return breakWord(s, dict, 0, dp);
+        int [] dp=new int[s.length()];
+        Arrays.fill(dp,-1);
+        return isPossible(s,wordDict,0, dp);
+        
     }
-
-    public boolean breakWord(String s, Set<String> dict, int idx, int[] dp) {
-
-        if (idx == s.length()) {
+    public static boolean isPossible(String s,List<String>dict, int idx, int [] dp ){
+        if(idx==s.length()){
             return true;
         }
 
-        if (dp[idx] != -1) {
-            return dp[idx] == 1;
+        if(dp[idx]!=-1){
+            return dp[idx]==1;
         }
 
-        for (int end = idx + 1; end <= s.length(); end++) {
-            String word = s.substring(idx, end);
+        for(int end=idx+1; end<=s.length(); end++){
+            String word=s.substring(idx,end);
 
-            if (dict.contains(word) && breakWord(s, dict, end, dp)) {
-                dp[idx] = 1;
+            if(dict.contains(word) && isPossible(s,dict,end,dp)){
+                dp[idx]=1;
                 return true;
             }
         }
-
-        dp[idx] = 0;
+        dp[idx]=0;
         return false;
     }
 }
