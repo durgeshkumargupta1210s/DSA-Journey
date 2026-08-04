4a63f7dfa6fa1d966e37a7840f1a45fd9f82d833
Durgesh Kumar Gupta <158087807+durgeshkumargupta1210s@users.noreply.github.com>
2026-08-04 22:19:00 +0530

    Time: 38 ms (44.87%) | Memory: 49.6 MB (41.67%) - LeetSync

---
+++ 1617-stone-game-iv/stone-game-iv.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean winnerSquareGame(int n) {
+
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, -1);
+
+        return solve(n, dp);
+    }
+
+    public static boolean solve(int n, int[] dp) {
+
+        if (n == 0)
+            return false;
+
+        if (dp[n] != -1) {
+            return dp[n] == 1;
+        }
+
+        for (int i = 1; i * i <= n; i++) {
+
+            if (!solve(n - i * i, dp)) {
+                dp[n] = 1;
+                return true;
+            }
+        }
+
+        dp[n] = 0;
+        return false;
+    }
+}
