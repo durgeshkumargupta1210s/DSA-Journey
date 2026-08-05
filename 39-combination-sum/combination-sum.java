cfef13859a1132b61b9014dffae94481a9f7f756
Durgesh Kumar Gupta <158087807+durgeshkumargupta1210s@users.noreply.github.com>
2026-08-05 19:21:02 +0530

    Time: 2 ms (96.30%) | Memory: 45.8 MB (33.79%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,21 +1,26 @@
 class Solution {
+    static List<List<Integer>>list;
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<Integer> ll=new ArrayList<>();
-		List<List<Integer>> ans=new ArrayList<>();
-		combination(candidates,target,ll,0,ans);
-        return ans;
+        list=new ArrayList<>();
+        solve(candidates,target,new ArrayList<>(),0);
+        return list;
+        
     }
-    private static void combination(int[] coin, int amount, List<Integer> ll,int idx,          List<List<Integer>> ans) {
-		if(amount==0) {
-			ans.add(new ArrayList(ll));
-			return;
-		}
-		for(int i=idx; i<coin.length; i++) {
-			if(amount>=coin[i]) {
-				ll.add(coin[i]);
-				combination(coin,amount-coin[i],ll,i,ans);
-				ll.remove(ll.size()-1);		
-            }
-		}
-	}
+    public static void solve(int [] nums, int target, List<Integer> curr, int idx){
+        if(target==0){
+            list.add(new ArrayList<>(curr));
+            return;
+        }
+        if(idx==nums.length){
+            return;
+        }
+
+        solve(nums,target,curr,idx+1);
+
+        if(target>=nums[idx]){
+            curr.add(nums[idx]);
+            solve(nums,target-nums[idx],curr,idx);
+            curr.remove(curr.size()-1);
+        }
+    }
 }
