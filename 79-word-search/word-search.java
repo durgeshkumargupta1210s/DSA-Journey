adc176483c722974471abf463e64d37d077230ff
Durgesh Kumar Gupta <158087807+durgeshkumargupta1210s@users.noreply.github.com>
2026-08-05 22:19:47 +0530

    Time: 140 ms (47.56%) | Memory: 42.9 MB (76.32%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,38 +1,38 @@
 class Solution {
+    static boolean [][] visited;
     public boolean exist(char[][] board, String word) {
-        int m=board.length;
-        int n=board[0].length;
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
+        int n=board.length;
+        int m=board[0].length;
+        visited=new boolean[n][m];
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
                 if(board[i][j]==word.charAt(0)){
-                    if(isWordExist(i,j,board,word,0)){
+                    if(solve(i,j,board,word,0)){
                         return true;
                     }
                 }
             }
         }
         return false;
-        
     }
-    public static boolean isWordExist(int cr, int cc, char[][] board, String word, int idx){
+    public static boolean solve(int cr, int cc, char[][] board, String word, int idx){
         if(idx==word.length()){
             return true;
         }
 
-        if(cr<0 || cc<0 || cr>=board.length || cc >=board[0].length || board[cr][cc]!=word.charAt(idx) ){
+        if(cr<0 || cc<0 || cr>=board.length || cc>=board[0].length || board[cr][cc]!=word.charAt(idx) || visited[cr][cc]==true){
             return false;
         }
 
-        char temp=board[cr][cc];
-        board[cr][cc]='#';
-
-        boolean found=isWordExist(cr+1,cc,board,word,idx+1)||
-                    isWordExist(cr-1,cc,board,word,idx+1)||
-                    isWordExist(cr,cc+1,board,word,idx+1)||
-                    isWordExist(cr,cc-1,board,word,idx+1);
-
-        board[cr][cc]=temp;
+        visited[cr][cc]=true;
+        boolean found=solve(cr+1,cc,board,word,idx+1)||
+                      solve(cr-1,cc,board,word,idx+1)||
+                      solve(cr,cc+1,board,word,idx+1)||
+                      solve(cr,cc-1,board,word,idx+1);
+        visited[cr][cc]=false;
 
         return found;
+
     }
+
 }
