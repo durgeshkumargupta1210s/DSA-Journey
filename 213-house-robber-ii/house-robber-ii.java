52b47c2c7c3f9742f60bb7adb900929afedd8ed2
Durgesh Kumar Gupta <158087807+durgeshkumargupta1210s@users.noreply.github.com>
2026-08-06 19:50:01 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (23.61%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,40 +1,32 @@
 class Solution {
     public int rob(int[] nums) {
-        int n = nums.length;
+        int n=nums.length;
 
-        // Edge case: only one house
-        if (n == 1) return nums[0];
-
-        int[] temp1 = new int[n - 1]; // Exclude last house
-        int[] temp2 = new int[n - 1]; // Exclude first house
-
-        // Fill temp1 with [0 .. n-2]
-        for (int i = 0; i < n - 1; i++) {
-            temp1[i] = nums[i];
+        if(n==1){
+            return nums[0];
         }
+        
+        int [] dp1=new int[n];
+        Arrays.fill(dp1,-1);
 
-        // Fill temp2 with [1 .. n-1]
-        for (int i = 1; i < n; i++) {
-            temp2[i - 1] = nums[i];
-        }
+        int [] dp2=new int[n];
+        Arrays.fill(dp2,-1);
 
-        return Math.max(RobberSO(temp1), RobberSO(temp2));
+        return Math.max(solve(nums,0,n-1, dp1), solve(nums,1,n, dp2));
+        
     }
-
-    public int RobberSO(int[] arr) {
-        if (arr.length == 1) return arr[0];
-
-        int prev2 = arr[0];
-        int prev = Math.max(arr[0], arr[1]);
-
-        for (int i = 2; i < arr.length; i++) {
-            int rob = arr[i] + prev2;
-            int dontRob = prev;
-            int curr = Math.max(rob, dontRob);
-            prev2 = prev;
-            prev = curr;
+    public static int solve(int [] nums, int idx, int end, int [] dp){
+        if(idx>=end){
+            return 0;
         }
 
-        return prev;
+        if(dp[idx]!=-1){
+            return dp[idx];
+        }
+
+        int skip=solve(nums,idx+1,end, dp);
+        int take=nums[idx]+solve(nums, idx+2,end, dp);
+
+        return dp[idx]= Math.max(skip,take);
     }
 }
