9bfcfd8b75fee8335d6dcb9f98338235da70399b
Durgesh Kumar Gupta <158087807+durgeshkumargupta1210s@users.noreply.github.com>
2026-08-06 23:30:03 +0530

    Time: 7 ms (80.10%) | Memory: 46.2 MB (57.25%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,28 +1,24 @@
 class Solution {
+
     public int lengthOfLIS(int[] nums) {
-        int [][] dp=new int[nums.length][nums.length+1];
-        for(int [] a : dp){
-            Arrays.fill(a,-1);
-        }
-        return solve(nums,0,-1, dp);
-        
-    }
-    public static int solve(int []nums,int idx, int prev, int [][] dp){
-        if(idx==nums.length){
-            return 0;
-        }
 
-        if(dp[idx][prev+1]!=-1){
-            return dp[idx][prev+1];
-        }
+        List<Integer> list = new ArrayList<>();
 
-        int skip=solve(nums,idx+1,prev, dp);
-        int take=0;
+        for (int num : nums) {
 
-        if(prev==-1 || nums[idx]>nums[prev]){
-            take=1+solve(nums,idx+1,idx, dp);
+            int idx = Collections.binarySearch(list, num);
+
+            if (idx < 0) {
+                idx = -(idx + 1);   // insertion point
+            }
+
+            if (idx == list.size()) {
+                list.add(num);
+            } else {
+                list.set(idx, num);
+            }
         }
 
-        return dp[idx][prev+1]= Math.max(take,skip);
+        return list.size();
     }
 }
