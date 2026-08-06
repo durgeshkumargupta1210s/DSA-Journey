855ada90860fe4aad775f370338e97e840981533
Durgesh Kumar Gupta <158087807+durgeshkumargupta1210s@users.noreply.github.com>
2026-08-06 23:25:43 +0530

    Time: 393 ms (9.01%) | Memory: 237.2 MB (13.58%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,66 +1,28 @@
 class Solution {
-
-    // Returns the length of the Longest Increasing Subsequence (LIS)
     public int lengthOfLIS(int[] nums) {
-
-        // dp[i] will store the minimum possible tail value
-        // of an increasing subsequence of length (i + 1)
-        int[] dp = new int[nums.length];
-
-        // Length of the LIS found so far
-        int len = 1;
-
-        // First element always forms a subsequence of length 1
-        dp[0] = nums[0];
-
-        // Iterate through the array starting from the second element
-        for (int i = 1; i < nums.length; i++) {
-
-            // Case 1: Current element extends the longest subsequence
-            if (nums[i] > dp[len - 1]) {
-                dp[len] = nums[i];
-                len++;
-            }
-
-            // Case 2: Current element replaces a larger element
-            // to maintain the smallest possible tail value
-            else {
-                int idx = binarySearch(dp, nums[i], len);
-                dp[idx] = nums[i];
-            }
+        int [][] dp=new int[nums.length][nums.length+1];
+        for(int [] a : dp){
+            Arrays.fill(a,-1);
         }
-
-        // Length of LIS
-        return len;
+        return solve(nums,0,-1, dp);
+        
     }
-
-    // Custom binary search to find the first index
-    // where dp[index] >= target (Lower Bound)
-    public int binarySearch(int[] dp, int target, int len) {
-
-        int low = 0;
-        int high = len - 1;
-
-        // Stores the position where target should be placed
-        int ans = 0;
-
-        while (low <= high) {
-
-            int mid = low + (high - low) / 2;
-
-            // If dp[mid] is greater than or equal to target,
-            // move left to find the first such position
-            if (dp[mid] >= target) {
-                ans = mid;
-                high = mid - 1;
-            }
-            // Otherwise, search in the right half
-            else {
-                low = mid + 1;
-            }
+    public static int solve(int []nums,int idx, int prev, int [][] dp){
+        if(idx==nums.length){
+            return 0;
         }
 
-        // Lower bound index
-        return ans;
+        if(dp[idx][prev+1]!=-1){
+            return dp[idx][prev+1];
+        }
+
+        int skip=solve(nums,idx+1,prev, dp);
+        int take=0;
+
+        if(prev==-1 || nums[idx]>nums[prev]){
+            take=1+solve(nums,idx+1,idx, dp);
+        }
+
+        return dp[idx][prev+1]= Math.max(take,skip);
     }
 }
