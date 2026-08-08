9cd4ebd5fc8df85d0bf75bf7d4b329fd2139b948
Durgesh Kumar Gupta <158087807+durgeshkumargupta1210s@users.noreply.github.com>
2026-08-08 22:01:32 +0530

    Time: 30 ms (14.04%) | Memory: 56 MB (5.10%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,60 +1,34 @@
 class Solution {
-
-    // Entry method that initializes DP table
+    static int [][] dp;
     public int longestCommonSubsequence(String text1, String text2) {
-
-        // dp[i][j] stores the LCS length of substrings
-        // text1[i...] and text2[j...]
-        int[][] dp = new int[text1.length() + 1][text2.length() + 1];
-
-        // Initialize DP table with -1 to indicate uncomputed states
-        for (int[] arr : dp) {
-            Arrays.fill(arr, -1);
+        dp=new int[text1.length()][text2.length()];
+        for(int [] a: dp){
+            Arrays.fill(a,-1);
         }
 
-        // Start recursion from index 0 of both strings
-        return lcs(text1, text2, 0, 0, dp);
+        return solve(text1,text2,0,0);
+        
     }
-
-    // Variable to store result for current recursive call
-    int ans = 0;
-
-    // Recursive function to compute LCS length
-    public int lcs(String s1, String s2, int i, int j, int[][] dp) {
-
-        // Base case:
-        // If either string is fully traversed,
-        // no common subsequence can be formed
-        if (i == s1.length() || j == s2.length()) {
+    static int ans=0;
+    public static int solve(String t1, String t2, int i, int j){
+        if(i==t1.length() || j==t2.length()){
             return 0;
         }
 
-        // If this subproblem is already solved, return stored value
-        if (dp[i][j] != -1) {
+        if(dp[i][j]!=-1){
             return dp[i][j];
         }
 
-        // Case 1: Current characters match
-        if (s1.charAt(i) == s2.charAt(j)) {
+        if(t1.charAt(i)==t2.charAt(j)){
+            ans=1+solve(t1,t2,i+1,j+1);
+        }
+        else{
+            int f=solve(t1,t2,i+1,j);
+            int s=solve(t1,t2,i,j+1);
 
-            // Include this matching character and move both pointers
-            ans = 1 + lcs(s1, s2, i + 1, j + 1, dp);
+            ans=Math.max(f,s);
         }
 
-        // Case 2: Characters do not match
-        else {
-
-            // Option 1: Skip character from second string
-            int f = lcs(s1, s2, i, j + 1, dp);
-
-            // Option 2: Skip character from first string
-            int s = lcs(s1, s2, i + 1, j, dp);
-
-            // Take maximum of both options
-            ans = Math.max(f, s);
-        }
-
-        // Store result in DP table and return
-        return dp[i][j] = ans;
+        return dp[i][j]= ans;
     }
 }
