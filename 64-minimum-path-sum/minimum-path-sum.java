04bd63a2918d23ad2ef5c553058ba756cdd46e5f
Durgesh Kumar Gupta <158087807+durgeshkumargupta1210s@users.noreply.github.com>
2026-08-08 21:05:07 +0530

    Time: 2 ms (78.08%) | Memory: 50.3 MB (56.92%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -1,28 +1,31 @@
 class Solution {
     public int minPathSum(int[][] grid) {
+
         int n = grid.length;
         int m = grid[0].length;
+
         int[][] dp = new int[n][m];
-        
+
         // Initialize dp with -1
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 dp[i][j] = -1;
             }
         }
-        
+
         return Minimum_Path(grid, 0, 0, dp);
     }
-    
+
     public static int Minimum_Path(int[][] grid, int cr, int cc, int[][] dp) {
-        // Base case: reached bottom-right cell
-        if (cr == grid.length - 1 && cc == grid[0].length - 1) {
-            return grid[cr][cc];
-        }
 
         // Out of bounds
         if (cr >= grid.length || cc >= grid[0].length) {
-            return Integer.MAX_VALUE;
+            return Integer.MAX_VALUE/2;
+        }
+
+        // Base case: reached bottom-right
+        if (cr == grid.length - 1 && cc == grid[0].length - 1) {
+            return grid[cr][cc];
         }
 
         // If already computed
@@ -31,11 +34,14 @@ class Solution {
         }
 
         // Move down
-        int d = Minimum_Path(grid, cr + 1, cc, dp);
+        int d = grid[cr][cc] +
+                Minimum_Path(grid, cr + 1, cc, dp);
+
         // Move right
-        int r = Minimum_Path(grid, cr, cc + 1, dp);
+        int r = grid[cr][cc] +
+                Minimum_Path(grid, cr, cc + 1, dp);
 
         // Store result in dp
-        return dp[cr][cc] = Math.min(d, r) + grid[cr][cc];
+        return dp[cr][cc] = Math.min(d, r);
     }
 }
